ed62ac2d863bdeb4f5e00052f9c49431348ddf44
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-04-12 18:09:06 +0100

    use new output.json format

--- embed-assets/run.js
+++ embed-assets/run.js
@@ -2,35 +2,37 @@
 const assert = require('assert');
 
 module.exports = function (Elm, output) {
-    const { ports: portSubscriptions = {}, flags } = output;
+    const { ports = [], flags } = output;
     const app = Elm.Main.init(flags !== undefined ? { flags } : undefined);
-    const subscriptionCounts = {};
+    let portEventIndex = 0;
 
     function send(portName, data) {
         app.ports[portName].send(data);
     }
 
     if (app.ports !== undefined) {
-        for (let portName of Object.keys(app.ports)) {
+        for (const portName of Object.keys(app.ports)) {
             if (app.ports[portName].subscribe !== undefined) {
-                subscriptionCounts[portName] = 0;
                 app.ports[portName].subscribe(data => {
-                    const index = subscriptionCounts[portName];
-                    subscriptionCounts[portName] += 1;
                     assert(
-                        Array.isArray(portSubscriptions[portName]),
-                        `port "${portName}" has been called by elm but should never be.`,
+                        portEventIndex < ports.length,
+                        `There should be exactly "${ports.length}" port events but this is event ${portEventIndex + 1}.`,
                     );
-                    const expectedNumberOfMsgs = portSubscriptions[portName].length;
+                    const [type, expectedName, expectedData] = ports[portEventIndex];
                     assert(
-                        index < expectedNumberOfMsgs,
-                        `port "${portName}" has been called ${subscriptionCounts[portName]} times by elm but should only have been called ${expectedNumberOfMsgs} times.`,
+                        type === "command",
+                        `Port event ${portEventIndex} should be a ${type} but command ${portName} received (with value ${data}).`,
+                    );
+                    assert(
+                        expectedName === portName,
+                        `Port event ${portEventIndex} should be command ${expectedName} but command ${portName} received (with value ${data}).`,
                     );
                     assert.deepStrictEqual(
                         data,
-                        portSubscriptions[portName][index],
-                        `Wrong data sent to port ${portName} on occurance ${index}`,
+                        expectedData,
+                        `Wrong data sent to port ${portName} during port event ${portEventIndex + 1}`,
                     )
+                    portEventIndex += 1;
                 })
             }
         }
@@ -38,13 +40,10 @@ module.exports = function (Elm, output) {
 
     process.on('exit', () => {
         if (app.ports !== undefined) {
-            for (let portName of Object.keys(app.ports)) {
-                const expectedNumberOfMsgs = portSubscriptions[portName] !== undefined ? portSubscriptions[portName].length : 0;
-                assert(
-                    subscriptionCounts[portName] == expectedNumberOfMsgs,
-                    `port ${portName} has been called ${subscriptionCounts[portName]} times but should be called exactly ${expectedNumberOfMsgs} times.`,
-                );
-            }
+            assert(
+                portEventIndex == ports.length,
+                `There have been ${portEventIndex} port events but should have been exactly ${ports.length} port events.`,
+            );
         }
     });
 
