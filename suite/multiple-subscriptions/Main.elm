3fd94f8d9d2fc7e4bf70507d5dfce6b32b2fe73b
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-05-29 18:18:56 +0100

    add multi sub suite

---
+++ suite/multiple-subscriptions/Main.elm
@@ -0,0 +1,34 @@
+port module Main exposing (main)
+
+import Platform
+import Util.Cmds
+import Util.Subs
+
+
+type Msg
+    = Read String
+    | DifferentRead String
+
+
+port differentRead : (String -> msg) -> Sub msg
+
+
+main : Platform.Program () () Msg
+main =
+    Platform.worker
+        { init = \() -> ( (), Cmd.none )
+        , update = update
+        , subscriptions = \() -> Util.Subs.read Read
+        }
+
+
+update : Msg -> () -> ( (), Cmd Msg )
+update msg () =
+    ( ()
+    , case msg of
+        Read _ ->
+            Util.Cmds.write "1"
+
+        DifferentRead _ ->
+            Util.Cmds.write "2"
+    )
