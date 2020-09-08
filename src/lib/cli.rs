224da7276834fbbb0d712487b58b13d358c7dc16
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-09-08 23:34:32 +0100

    upgrade to clap 3

--- src/lib/cli.rs
+++ src/lib/cli.rs
@@ -1,9 +1,49 @@
 use crate::lib::config;
 use crate::lib::config::Config;
-use clap::App;
-use clap::Arg;
+use clap::Clap;
 use std::fs::File;
 use std::path::PathBuf;
+
+#[derive(Clap)]
+#[clap(
+    version = "0.0.2",
+    author = "Harry Sarson <harry.sarson@hotmail.co.uk>",
+    about = "Test suite for an elm compiler",
+    group=clap::ArgGroup::new("suite_or_suites").required(true)
+)]
+struct Opts {
+    /// Sets a custom config file. Could have been an Option<T> with no default too
+    #[clap(short, long, about = "Set config file")]
+    config: Option<PathBuf>,
+
+    #[clap(
+        long,
+        value_name = "DIRECTORY",
+        about = "The suite to test",
+        group = "suite_or_suites"
+    )]
+    suite: Option<PathBuf>,
+
+    #[clap(
+        long,
+        value_name = "DIRECTORY",
+        about = "A directory containing suites to test",
+        group = "suite_or_suites",
+        conflicts_with = "out-dir"
+    )]
+    suites: Option<PathBuf>,
+
+    #[clap(
+        long,
+        value_name = "DIRECTORY",
+        about = "The directory to place built files in."
+    )]
+    out_dir: Option<PathBuf>,
+    #[clap(long, value_name = "FILE", about = "Dump the configuration to FILE.")]
+    show_config: Option<PathBuf>,
+    #[clap(long, about = "Stop running on the first failed suite.")]
+    fail_fast: bool,
+}
 
 pub enum Task {
     DumpConfig(PathBuf),
@@ -21,62 +61,19 @@ pub struct Instructions {
 }
 
 pub fn get_cli_task() -> Instructions {
-    let matches = App::new("Elm Torture")
-        .version("0.0.1")
-        .author("Harry Sarson <harry.sarson@hotmail.co.uk>")
-        .about("Test suite for an elm compiler")
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Set config file")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("suite")
-                .long("suite")
-                .value_name("DIRECTORY")
-                .help("The suite to test")
-                .required(true)
-                .conflicts_with("suites")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("suites")
-                .long("suites")
-                .value_name("DIRECTORY")
-                .help("A directory containing suites to test")
-                .conflicts_with("out_dir")
-                .takes_value (true))
-        .arg(
-            Arg::with_name("out_dir")
-                .long("out-dir")
-                .value_name("DIRECTORY")
-                .help("The directory to place built files in.\nMust not exist or be an empty directory")
-                .takes_value(true)  )
-        .arg(
-            Arg::with_name("show_config")
-                .long("show-config")
-                .value_name("FILE")
-                .help("Dump the configuration to FILE")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("fail_fast")
-                .long("fail-fast")
-                .help("Stop running on the first failed suite."),
-        )
-        .get_matches();
-
-    let fail_fast = matches.is_present("fail_fast");
+    let Opts {
+        suite,
+        suites,
+        out_dir,
+        config,
+        fail_fast,
+        show_config,
+    } = Opts::parse();
 
     let config: Config<PathBuf> = {
-        let config_file = matches.value_of_os("config");
-
-        config_file
+        config
             .map(|p| {
-                let file = File::open(p).expect("config file not found");
+                let file = File::open(&p).expect("config file not found");
                 let config: config::Config<config::RelativePath> = serde_json::from_reader(file)
                     .expect("error while reading json configuration file");
                 config.into_config(p)
@@ -87,25 +84,17 @@ pub fn get_cli_task() -> Instructions {
     Instructions {
         config,
         fail_fast,
-        task: if let Some(config_file) = matches.value_of("show_config") {
-            Task::DumpConfig(config_file.into())
-        } else if let Some(suites) = matches.value_of("suites") {
-            Task::RunSuites(
-                suites
-                    .parse()
-                    .unwrap_or_else(|infalible| match infalible {}),
-            )
-        } else {
-            Task::RunSuite {
-                suite: matches
-                    .value_of("suite")
-                    .unwrap()
-                    .parse()
-                    .unwrap_or_else(|infalible| match infalible {}),
-                out_dir: matches
-                    .value_of("out_dir")
-                    .map(|dir| dir.parse().unwrap_or_else(|infalible| match infalible {})),
-            }
-        },
+        task: show_config.map_or_else(
+            || {
+                suites.map_or_else(
+                    || Task::RunSuite {
+                        suite: suite.unwrap(),
+                        out_dir,
+                    },
+                    Task::RunSuites,
+                )
+            },
+            Task::DumpConfig,
+        ),
     }
 }
