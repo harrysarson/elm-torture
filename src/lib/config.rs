c7bdabdaa923c52c80a4cc770af904d58d149f2a
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-09-18 09:58:50 +0100

    tidy up

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,21 +1,9 @@
 use clap::Clap;
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::str::FromStr;
 use std::string::String;
 use std::time::Duration;
 use std::{fmt, num::NonZeroI32};
-use std::{path::Path, str::FromStr};
-
-// fn xxx() -> {
-
-//     if let Some(config_dir) = config_file.map(Path::new).and_then(Path::parent) {
-//         deserialised.allowed_failures = deserialised
-//             .allowed_failures()
-//             .iter()
-//             .map(|p| config_dir.join(p))
-//             .collect();
-//     }
-// }
 
 #[derive(Debug, Deserialize, Serialize, Clap, PartialEq, Eq, Clone, Copy)]
 #[serde(rename_all = "kebab-case")]
@@ -51,22 +39,6 @@ pub struct Config {
         parse(try_from_str = humantime::parse_duration)
     )]
     run_timeout: Option<Duration>,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(transparent)]
-pub struct RelativePath(PathBuf);
-
-impl AsRef<Path> for RelativePath {
-    fn as_ref(&self) -> &Path {
-        self.0.as_ref()
-    }
-}
-
-impl AsRef<RelativePath> for RelativePath {
-    fn as_ref(&self) -> &RelativePath {
-        &self
-    }
 }
 
 impl Config {
